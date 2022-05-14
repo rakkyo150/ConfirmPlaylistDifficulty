c0081c307f774f287a78bf0e81332f85de294ef8
yatakabs <87169907+yatakabs@users.noreply.github.com>
2022-05-14 19:34:31 +0900

    Fixed a bug where BeatSaber UI collapses when PlayButton's gameObject is null.

--- ConfirmPlaylistDifficulty/DataModel.cs
+++ ConfirmPlaylistDifficulty/DataModel.cs
@@ -1,4 +1,5 @@
-﻿using BeatSaberPlaylistsLib.Types;
+﻿using System.Linq;
+using BeatSaberPlaylistsLib.Types;
 using HMUI;
 using IPA.Utilities;
 using UnityEngine;
@@ -11,63 +12,59 @@ namespace ConfirmPlaylistDifficulty
         internal static Color defaultColor;
         internal static Button _actionButton;
         internal static IDifficultyBeatmap difficultyBeatmap;
-        internal static SongDetailsCache.Structs.MapCharacteristic characteristic;
         internal static IPlaylistSong playlistSong = null;
+        internal static SelectLevelCategoryViewController.LevelCategory selectedLevelCategory;
 
         public static void RefreshButtonColor()
         {
-            // playlistSongのキャッシュがある場合
-            if (DataModel.playlistSong != null && DataModel.difficultyBeatmap != null && DataModel.characteristic != null)
-            {
-                if (DataModel.playlistSong.levelID == DataModel.difficultyBeatmap.level.levelID)
-                {
-                    // プレイリストの難易度指定が無い場合
-                    if (DataModel.playlistSong.Difficulties == null)
-                    {
-                        ChangeStartButtonColor(false);
-                        return;
-                    }
-                    // プレイリストの難易度指定がある場合
-                    else
-                    {
-                        foreach (var difficulty in DataModel.playlistSong.Difficulties)
-                        {
-                            // プレイリストの難易度を選択している場合
-                            if (difficulty.Characteristic == DataModel.characteristic.ToString()
-                                && difficulty.BeatmapDifficulty == DataModel.difficultyBeatmap.difficulty)
-                            {
-                                ChangeStartButtonColor(false);
-                                return;
-                            }
-                        }
+            var isTargetLevelSelected = playlistSong?.levelID == difficultyBeatmap?.level?.levelID;
 
-                        // プレイリストの難易度を選択していない場合
-                        ChangeStartButtonColor(true);
-                        return;
-                    }
-                }
+            // プレイリストの難易度を選択していれば true
+            var selectedCharasteristic = difficultyBeatmap?.parentDifficultyBeatmapSet?.beatmapCharacteristic?.serializedName;
 
-                ChangeStartButtonColor(false);
-            }
+            var isTargetDifficultySelected =
+                selectedCharasteristic != null &&
+                playlistSong
+                    ?.Difficulties
+                    ?.Any(x => x.Characteristic == selectedCharasteristic && x.BeatmapDifficulty == difficultyBeatmap?.difficulty)
+                    == true;
+
+            var shouldWarn = isTargetLevelSelected && !isTargetDifficultySelected;
+            ChangeStartButtonColor(toWarningColor: shouldWarn);
         }
 
-        public static void ChangeStartButtonColor(bool red)
+        public static void ChangeStartButtonColor(bool toWarningColor)
         {
-            foreach (var bg in DataModel._actionButton.GetComponentsInChildren<ImageView>())
+            if (DataModel._actionButton?.IsDestroyed() != false)
             {
-                if (bg.name == "BG")
-                {
-                    if (red)
-                    {
-                        bg.color = Color.red;
-                        bg.SetField("_gradient", false);
-                    }
-                    else
-                    {
-                        bg.color = DataModel.defaultColor;
-                        bg.SetField("_gradient", true);
-                    }
-                }
+                Plugin.Log.Warn($"Action button is destroyed. Skipping start button color change.");
+                return;
+            }
+
+            if (DataModel._actionButton.gameObject == null)
+            {
+                Plugin.Log.Warn($"DataModel._actionButton is null. Skipping start button color change.");
+                return;
+            }
+
+            var bg = DataModel._actionButton.GetComponentsInChildren<ImageView>()
+                ?.SingleOrDefault(x => x.name == "BG");
+
+            if (bg == null)
+            {
+                Plugin.Log.Warn("Failed to find BG ImageView.");
+                return;
+            }
+
+            if (toWarningColor)
+            {
+                bg.color = Color.red;
+                bg.SetField("_gradient", false);
+            }
+            else
+            {
+                bg.color = DataModel.defaultColor;
+                bg.SetField("_gradient", true);
             }
         }
     }
--- ConfirmPlaylistDifficulty/HarmonyPatches/Patches.cs
+++ ConfirmPlaylistDifficulty/HarmonyPatches/Patches.cs
@@ -1,9 +1,7 @@
-﻿using ConfirmPlaylistDifficulty.Configuration;
+﻿using System.Linq;
+using ConfirmPlaylistDifficulty.Configuration;
 using HarmonyLib;
 using HMUI;
-using IPA.Utilities;
-using System;
-using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -24,46 +22,35 @@ namespace ConfirmPlaylistDifficulty.HarmonyPatches
         /// <param name="arg1">The Parameter1Type arg1 that was passed to MethodToPatch</param>
         /// <param name="____privateFieldInClassToPatch">Reference to the private field in ClassToPatch named '_privateFieldInClassToPatch', 
         ///     added three _ to the beginning to reference it in the patch. Adding ref means we can change it.</param>
-        static void Postfix(IBeatmapLevel ____level, IDifficultyBeatmap ____selectedDifficultyBeatmap, LevelParamsPanel ____levelParamsPanel, StandardLevelDetailView __instance)
+        private static void Postfix(IBeatmapLevel ____level, IDifficultyBeatmap ____selectedDifficultyBeatmap, LevelParamsPanel ____levelParamsPanel, StandardLevelDetailView __instance)
         {
-            DataModel._actionButton = __instance.actionButton;
-
-            foreach (var bg in DataModel._actionButton.GetComponentsInChildren<ImageView>())
-            {
-                if (bg.name == "BG" && DataModel.defaultColor == new Color(0f, 0f, 0f, 0f))
-                {
-                    DataModel.defaultColor = bg.color;
-                }
-            }
+            var actionButton = DataModel._actionButton = __instance.actionButton;
 
             DataModel.difficultyBeatmap = ____selectedDifficultyBeatmap;
-            Enum.TryParse(DataModel.difficultyBeatmap.parentDifficultyBeatmapSet.beatmapCharacteristic.serializedName, out DataModel.characteristic);
+            DataModel.selectedLevelCategory = Resources.FindObjectsOfTypeAll<SelectLevelCategoryViewController>()
+                .FirstOrDefault()
+                ?.selectedLevelCategory
+                ?? SelectLevelCategoryViewController.LevelCategory.None;
 
-            SelectLevelCategoryViewController selectLevelCategoryViewController = Resources.FindObjectsOfTypeAll<SelectLevelCategoryViewController>().First();
+            if (actionButton?.gameObject == null)
+            {
+                Plugin.Log.Error("Failed to obtain actionButton.");
+                return;
+            }
+
+
+            if (DataModel.defaultColor == default)
+            {
+                DataModel.defaultColor = actionButton
+                        .GetComponentsInChildren<ImageView>()
+                        .SingleOrDefault(x => x.name == "BG")
+                        ?.color
+                        ?? default;
+            }
 
             if (PluginConfig.Instance.Enable)
             {
-                if (selectLevelCategoryViewController.selectedLevelCategory != SelectLevelCategoryViewController.LevelCategory.CustomSongs)
-                {
-                    if (DataModel.defaultColor != null && DataModel._actionButton != null)
-                    {
-                        foreach (var bg in DataModel._actionButton.GetComponentsInChildren<ImageView>())
-                        {
-                            if (bg.name == "BG")
-                            {
-                                if (bg.color == Color.red)
-                                {
-                                    bg.color = DataModel.defaultColor;
-                                    bg.SetField("_gradient", true);
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    DataModel.RefreshButtonColor();
-                }
+                DataModel.RefreshButtonColor();
             }
         }
     }
